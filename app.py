374d3d2b7bd0176b6fbd73f59b2bbba7ef048001
Bill Davis <bill.davis@gmail.com>
2020-06-04 04:58:15 +0000

    Changes to connect to quotations database

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
  
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'tasks_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:r00tUser@cluster0-oxfue.mongodb.net/tasks_manager?retryWrites=true&w=majority')
+app.config["MONGO_DBNAME"] = 'quotations_space'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:r00tUser@cluster0-oxfue.mongodb.net/quotations_space?retryWrites=true&w=majority')
  
 mongo = PyMongo(app)
  
@@ -13,7 +13,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_quotes)
 def get_tasks():
-    return render_template("quotations.html", tasks=mongo.db.quotations.find())
+    return render_template("quotes.html", tasks=mongo.db.quotations.find())
  
  
 if __name__ == '__main__':
