26b72932c056064667e1c5c32071fab421072485
Bill Davis <bill.davis@gmail.com>
2020-06-21 00:58:01 +0000

    Added edittask form and decorator, added search field, did some CSS formatting, added animated starfield in header

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'quotations_space'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:r00tUser@cluster0-oxfue.mongodb.net/quotations_space?retryWrites=true&w=majority')
+
+# 'mongodb+srv://root:r00tUser@cluster0-oxfue.mongodb.net/quotations_space?retryWrites=true&w=majority'
+# 'mongodb://localhost'
 
 mongo = PyMongo(app)
 
@@ -13,14 +17,14 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_quotes')
 def get_quotes():
-    return render_template("quotes.html", 
-                            quotations=mongo.db.quotations.find())
+    return render_template('quotes.html',
+                           quotations=mongo.db.quotations.find())
 
 
 @app.route('/add_quote')
 def add_quote():
     return render_template('addquote.html',
-                            categories=mongo.db.categories.find())
+                           categories=mongo.db.categories.find())
 
 
 @app.route('/insert_quote', methods=['POST'])
@@ -28,6 +32,15 @@ def insert_quote():
     quotes = mongo.db.quotations
     quotes.insert_one(request.form.to_dict())
     return redirect(url_for('get_quotes'))
+
+
+
+@app.route('/edit_quote/<quote_id>')
+def edit_quote(quote_id):
+    the_quote = mongo.db.quotations.find_one({"_id": ObjectId(quote_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('editquote.html', quote=the_quote,
+                           categories=all_categories)
 
 
 if __name__ == '__main__':
--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 # commands to set up the workspace 
 pip3 install flask
+pip3 install flask-pymongo
+pip3 install dnspython
+
 # add other installers here
+
+
 pip3 freeze > requirements.txt
