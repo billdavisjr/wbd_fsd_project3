8fb45d6c7e7a4d81dd3991d49c8774fc6b92b6ee
Bill Davis <bill.davis@gmail.com>
2020-06-22 01:03:57 +0000

    Heading styling c hanges, quote text size enlargement

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
-# commands to set up the workspace 
+# Commands to set up the workspace 
+
+# INSTALL PYTHON PACAKAGES & CREATE REQUIREMENTS FILE
+
 pip3 install flask
 pip3 install flask-pymongo
 pip3 install dnspython
-
 # add other installers here
-
-
 pip3 freeze > requirements.txt
 
-heroku git:remote quotations-space
+# HEROKU SETUP
 
+heroku git:remote quotations-space
+heroku domains:add 'quotations-space'
